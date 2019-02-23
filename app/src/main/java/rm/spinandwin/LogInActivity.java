d19dfc9e2bc2b18e78c12313a416af7a9a0a0e9e
rohit <rohitmaria100@gmail.com>
2019-02-23 16:41:38 +0530

    Login page is done

--- app/src/main/java/rm/spinandwin/LogInActivity.java
+++ app/src/main/java/rm/spinandwin/LogInActivity.java
@@ -1,42 +1,25 @@
 package rm.spinandwin;
 
 import android.app.Dialog;
-import android.os.Handler;
-import android.os.StrictMode;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Map;
 
 import rm.spinandwin.helper.Api;
 import rm.spinandwin.helper.H;
 import rm.spinandwin.helper.Json;
 import rm.spinandwin.helper.LoadingDialog;
+import rm.spinandwin.helper.Session;
 import rm.spinandwin.helper.Static;
 
-import static android.os.Build.VERSION_CODES.P;
-
-public class LogInActivity extends AppCompatActivity
-{
+public class LogInActivity extends AppCompatActivity {
     private LoadingDialog loadingDialog;
+    private Dialog dialog;
+    private Json mainJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,58 +30,114 @@ public class LogInActivity extends AppCompatActivity
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-               setUpJson();
+            public void onClick(View v) {
+                setUpJson();
             }
         });
     }
 
-    private void setUpJson()
-    {
-        Json json = new Json();
-        String string= ((EditText)findViewById(R.id.userName)).getText().toString().trim();
+    private void setUpJson() {
+        mainJson = new Json();
+        String string = ((EditText) findViewById(R.id.userName)).getText().toString().trim();
         if (string.isEmpty()) {
             Toast.makeText(LogInActivity.this, "Username is mandatory.", Toast.LENGTH_SHORT).show();
             return;
-        }
-        else if (string.length()>1 && string.length()<10) {
+        } else if (string.length() > 1 && string.length() < 10) {
             Toast.makeText(LogInActivity.this, "Enter valid user name", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        json.addString("login_id",string);
+        mainJson.addString("login_id", string);
 
-        string = ((EditText)findViewById(R.id.password)).getText().toString().trim();
+        string = ((EditText) findViewById(R.id.password)).getText().toString().trim();
         if (string.isEmpty()) {
             Toast.makeText(LogInActivity.this, "Password is mandatory.", Toast.LENGTH_SHORT).show();
             return;
         }
+        mainJson.addString("login_password", string);
 
-        json.addString("login_password",string);
-
-        //hitLogInApi(json);
-        otpPopUp();
+        hitLogInApi(mainJson);
     }
 
-    private void otpPopUp() {
-        final Dialog dialog = new Dialog(this);
+    private void hitLogInApi(Json json) {
+        Api.newApi(this, Static.baseUrl + "GamerLogin").addJson(json)
+                .setMethod(Api.POST)
+                .onLoading(new Api.OnLoadingListener() {
+                    @Override
+                    public void onLoading(boolean isLoading) {
+                        if (isLoading)
+                            loadingDialog.show("loading...");
+                        else
+                            loadingDialog.dismiss();
+                    }
+                })
+                .onError(new Api.OnErrorListener() {
+                    @Override
+                    public void onError() {
+                        H.showMessage(LogInActivity.this, "Something went wrong.");
+                    }
+                })
+                .onSuccess(new Api.OnSuccessListener() {
+                    @Override
+                    public void onSuccess(Json json) {
+                        if (json.getString(Static.status).equalsIgnoreCase("100")) {
+                            Session session = new Session(LogInActivity.this);
+                            json = json.getJson(Static.data);
+                            if (json.getString(Static.IsFirstTimeLogin).equalsIgnoreCase("1")) {
+                                String otp = json.getString(Static.OTP);
+                                if (otp != null) {
+                                    session.addString(Static.OTP, otp);
+                                    otpPopUpDialog();
+                                }
+
+                            } else if (json.getString(Static.IsFirstTimeLogin).equalsIgnoreCase("0"))
+                            {
+                                session.addString(Static.data, json + "");
+                                startActivity(new Intent(LogInActivity.this,MainActivity.class));
+                                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                                finish();
+                            }
+                        } else
+                            H.showMessage(LogInActivity.this, json.getString(Static.message));
+
+                    }
+                })
+                .run("logIn");
+    }
+
+    private void otpPopUpDialog() {
+        dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.otp_dialog);
         dialog.show();
-        dialog.setCancelable(false);
 
-        new Handler().postDelayed(new Runnable() {
+        dialog.findViewById(R.id.verifyOtp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otp = ((EditText) dialog.findViewById(R.id.otp)).getText().toString().trim();
+                String sessionOtp = new Session(LogInActivity.this).getString(Static.OTP);
+                if (sessionOtp != null)
+                {
+                    if (sessionOtp.equalsIgnoreCase(otp))
+                        hitOtpVerificationApi();
+                    else
+                        H.showMessage(LogInActivity.this, "Please enter valid OTP");
+                }
+            }
+        });
+
+        //dialog.setCancelable(false);
+
+       /* new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 dialog.hide();
             }
-        },7000);
+        },7000);*/
     }
 
-    private void hitLogInApi(Json json)
-    {
-        Api.newApi(this, Static.baseUrl + "GamerLogin").addJson(json)
+    private void hitOtpVerificationApi() {
+        Api.newApi(this, Static.baseUrl + "VerifyOTP").addJson(mainJson)
                 .setMethod(Api.POST)
                 .onLoading(new Api.OnLoadingListener() {
                     @Override
@@ -119,9 +158,15 @@ public class LogInActivity extends AppCompatActivity
                     @Override
                     public void onSuccess(Json json)
                     {
-                        H.log("jsonIs",""+json);
+                        if (json.getString(Static.status).equalsIgnoreCase("100"))
+                        {
+                            hitLogInApi(mainJson);
+                        } else
+                            H.showMessage(LogInActivity.this, json.getString(Static.message));
+
                     }
                 })
-                .run("logIn");
+                .run("otpVerification");
     }
+
 }
--- app/src/main/java/rm/spinandwin/helper/Static.java
+++ app/src/main/java/rm/spinandwin/helper/Static.java
@@ -4,5 +4,14 @@ public class Static {
 
     public static boolean show_log = true;
 
-    public static String baseUrl = "https://apispinwheel.spinwheels.info/";
+    public static String baseUrl = "http://apispinwheel.spinwheels.info/";
+
+
+    public static String status = "status";
+    public static String data = "data";
+    public static final String IsFirstTimeLogin = "IsFirstTimeLogin";
+    public static final String OTP = "OTP";
+
+
+    public static String message = "message";
 }
