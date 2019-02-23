7ae965bc5c469b2d516702bbeff57e3ebde802f2
rohit <rohitmaria100@gmail.com>
2019-02-24 00:33:30 +0530

    Main design is partially done.

--- app/src/main/java/rm/spinandwin/MainActivity.java
+++ app/src/main/java/rm/spinandwin/MainActivity.java
@@ -2,6 +2,12 @@ package rm.spinandwin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = findViewById(R.id.parentLayout);
+        View view;
+        for (int i =0; i<12; i++) {
+            view = View.inflate(this, R.layout.text_view_of_grid, null);
+            linearLayout.addView(view);
+        }
+
     }
+
 }
