88ee77b3b24a4c7cff7c767feb8b9f19d52b2d0d
rohit <rohitmaria100@gmail.com>
2019-02-24 17:33:04 +0530

    Designing is done,

--- app/src/main/java/rm/spinandwin/MainActivity.java
+++ app/src/main/java/rm/spinandwin/MainActivity.java
@@ -3,26 +3,72 @@ package rm.spinandwin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import rm.spinandwin.helper.H;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout = findViewById(R.id.parentLayout);
-        View view;
-        for (int i =0; i<12; i++) {
-            view = View.inflate(this, R.layout.text_view_of_grid, null);
-            linearLayout.addView(view);
-        }
 
+        setUpGrid(2);
     }
 
+    int count;
+
+    private void setUpGrid(int j) {
+        int k = j;
+        LinearLayout parentLayout = findViewById(R.id.parentLayout);
+        View view = View.inflate(this, R.layout.grid_elemet, null);
+        LinearLayout childLayout = (LinearLayout) view;
+        TextView textView;
+        String string;
+
+        if (k==2)
+        {
+            for (int i = 0; i < 12; i++) {
+                textView = ((TextView) childLayout.getChildAt(i));
+                string = textView.getText().toString().trim();
+
+                if (string.equals("6") || string.equals("15") || string.equals("24") || string.equals("33"))
+                {
+                    textView.setBackground(getResources().getDrawable(R.drawable.red_bg));
+                    textView.setTag(j);
+                }
+                else{
+                    textView.setBackground(getResources().getDrawable(R.drawable.black_bg));
+                    textView.setTag(j);
+                }
+                textView.setText(j + "");
+                j += 3;
+            }
+        }
+        else
+        {
+            for (int i=0;i<12;i++)
+            {
+                textView = ((TextView) childLayout.getChildAt(i));
+                string = textView.getText().toString().trim();
+                //int tag = (int) textView.getTag();
+                if (string.equals("6") || string.equals("12") || string.equals("15") || string.equals("24") || string.equals("30") || string.equals("33"))
+                    textView.setBackground(getResources().getDrawable(R.drawable.black_bg));
+                else
+                    textView.setBackground(getResources().getDrawable(R.drawable.red_bg));
+
+                textView.setText(j + "");
+                j += 3;
+            }
+        }
+        parentLayout.addView(childLayout);
+        count++;
+        if (count < 2)
+            setUpGrid(1);
+
+    }
 }
