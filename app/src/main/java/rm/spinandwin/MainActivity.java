9e87a8d8e990c191678eb16bb4b545f5d1f1215c
rohit <rohitmaria100@gmail.com>
2019-02-26 14:57:10 +0530

    transfer pop up is made  and done till id and tag assignment.

--- app/src/main/java/rm/spinandwin/MainActivity.java
+++ app/src/main/java/rm/spinandwin/MainActivity.java
@@ -1,26 +1,56 @@
 package rm.spinandwin;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import rm.spinandwin.helper.H;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        setUpGrid(2);
+        //setUpGrid(2);
     }
 
     int count;
+
+    public void onRowOneClick(View view)
+    {
+        if (view.getId() == R.id.transfer)
+        {
+            dialog = new Dialog(this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.transfer_layout);
+
+            dialog.findViewById(R.id.close).setOnClickListener(this);
+            dialog.findViewById(R.id.transfer).setOnClickListener(this);
+            dialog.show();
+        }
+    }
+
+    public void on1To36Click(View view)
+    {
+        H.log("clickIs",((TextView)view).getText().toString());
+        H.showMessage(this,((TextView)view).getText().toString());
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v.getId() == R.id.close)
+            dialog.hide();
+    }
 
     private void setUpGrid(int j) {
         int k = j;
@@ -37,15 +67,12 @@ public class MainActivity extends AppCompatActivity
                 string = textView.getText().toString().trim();
 
                 if (string.equals("6") || string.equals("15") || string.equals("24") || string.equals("33"))
-                {
                     textView.setBackground(getResources().getDrawable(R.drawable.red_bg));
-                    textView.setTag(j);
-                }
-                else{
+                else
                     textView.setBackground(getResources().getDrawable(R.drawable.black_bg));
-                    textView.setTag(j);
-                }
+
                 textView.setText(j + "");
+                textView.setTag(j + "");
                 j += 3;
             }
         }
@@ -55,13 +82,13 @@ public class MainActivity extends AppCompatActivity
             {
                 textView = ((TextView) childLayout.getChildAt(i));
                 string = textView.getText().toString().trim();
-                //int tag = (int) textView.getTag();
                 if (string.equals("6") || string.equals("12") || string.equals("15") || string.equals("24") || string.equals("30") || string.equals("33"))
                     textView.setBackground(getResources().getDrawable(R.drawable.black_bg));
                 else
                     textView.setBackground(getResources().getDrawable(R.drawable.red_bg));
 
                 textView.setText(j + "");
+                textView.setTag(j + "");
                 j += 3;
             }
         }
@@ -70,5 +97,11 @@ public class MainActivity extends AppCompatActivity
         if (count < 2)
             setUpGrid(1);
 
+    }
+
+    public void onBorderClick(View view)
+    {
+        H.log("borderClickIs",view.getTag().toString());
+        H.showMessage(this,view.getTag().toString());
     }
 }
